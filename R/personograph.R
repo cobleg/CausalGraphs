f0854d7fe5ae64038bebb7a93940d5a2d17fbe07
Grant Coble-Neal <grantcn96@gmail.com>
2022-10-22 19:34:47 +0800

    custom personograph

--- R/personograph.R
+++ R/personograph.R
@@ -5,9 +5,35 @@
 # Package source: https://github.com/joelkuiper/personograph
 # Reference: https://jtr13.github.io/cc21fall1/icons-and-symbols-in-r.html
 
+library(grImport)
 library(personograph)
+library(tidyverse)
 
 data <- list(first=0.89, second=0.06, third=0.05)
 # With colors
 personograph(data, n.icons=64, dimensions=c(8, 8), colors=list(first="grey", second="blue", third="red"),
              fig.title = "Example of a personograph")
+
+mtcars.1 <- mtcars %>% 
+  mutate(cylinders = as.factor(cyl)) %>% 
+  group_by(cylinders) %>%
+  summarise(cnt = n()) %>% 
+  mutate(proportions = round(cnt) / sum(cnt), 1) %>% 
+  arrange((desc(proportions))) %>% 
+  column_to_rownames("cylinders") %>% 
+  select(proportions)
+  
+rownames(mtcars.1) <- english::as.english(as.numeric(rownames(mtcars.1)))
+
+mtcars.2 <- mtcars.1 %>% 
+  rownames_to_column("cylinders") %>% 
+  pivot_wider(values_from = proportions, names_from = cylinders)
+
+grImport::PostScriptTrace(here::here("images", "car.eps"), outfilename = "car2.ps")
+car.1 <- grImport::readPicture("car2.ps")
+data <- as.list(mtcars.2)
+
+personograph(data, n.icons=100, dimensions=c(10, 10), colors=list(eight = "grey", four = "blue", six = "red"),
+             fig.title = "Cars by number of cylinders",
+             icon =  grImport::readPicture("car2.ps"))
+ 
